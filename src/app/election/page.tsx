fba96f8f0bba734f25d03ac797fd73b3aa7b9dc1
koudaihirata <2230051@ecc.ac.jp>
2023-12-10 00:36:47 +0900

    とりあえず２ページ目完成

--- src/app/election/page.tsx
+++ src/app/election/page.tsx
@@ -7,6 +7,7 @@ import Header_main from "@/components/header/main"
 import Image from "next/image"
 import "@/app/election/style.css"
 import Link from "next/link"
+import { type } from "os"
 
 
 
@@ -18,6 +19,12 @@ type CandidateProps = {
     num:Number,
     color:string,
 }
+type PartyProps = {
+    name:string,
+    num:Number,
+    color:string,
+}
+
 const persons = [
     {
         name: "平田広大",
@@ -47,25 +54,112 @@ const persons = [
         num:10042,
         color:"#657F10",
     },
-    {
-        name: "山口煙管",
-        party:"無",
-        now:"新",
-        num:5432,
-        color:"#D9D9D9",
-    },      
+    // {
+    //     name: "山口煙管",
+    //     party:"無",
+    //     now:"新",
+    //     num:5432,
+    //     color:"#D9D9D9",
+    // },      
 ]
+const parties = [
+    {
+        name:"自民",
+        num:Number("1825"),
+        color:"rgb(210, 35, 25)",
+    },
+    {
+        name:"維新",
+        num:Number("784"),
+        color:"rgb(225, 154, 0)",
+    },
+    {
+        name:"立民",
+        num:Number("677"),
+        color:"rgb(35, 145, 255)",
+    },
+    {
+        name:"公明",
+        num:Number("618"),
+        color:"rgb(235, 97, 190)",
+    },
+    {
+        name:"共産",
+        num:Number("361"),
+        color:"rgb(110, 65, 225)",
+    },
+    {
+        name:"国民",
+        num:Number("315"),
+        color:"rgb(0, 16, 165)",
+    },
+    {
+        name:"れ新",
+        num:Number("231"),
+        color:"rgb(240, 160, 167)",
+    },
+    {
+        name:"参政",
+        num:Number("176"),
+        color:"rgb(235, 100, 10)",
+    },
+    {
+        name:"社民",
+        num:Number("125"),
+        color:"rgb(5, 85, 90)",
+    },
+    {
+        name:"N党",
+        num:Number("125"),
+        color:"rgb(182, 200, 27)",
+    },
+    {
+        name:"ごぼ",
+        num:Number("19"),
+        color:"rgb(35, 70, 0)",
+    },
+    {
+        name:"幸福",
+        num:Number("14"),
+        color:"rgb(150, 150, 250)",
+    },
+    {
+        name:"第一",
+        num:Number("10"),
+        color:"rgb(170, 130, 75)",
+    },
+    {
+        name:"くに",
+        num:Number("7"),
+        color:"rgb(180, 110, 90)",
+    },
+    {
+        name:"新風",
+        num:Number("6"),
+        color:"rgb(100, 50, 10)",
+    },
+]
+
 export function Candidate(props:CandidateProps) {
     return(
         <div className="candidate">
             <Image src='/img/profile.svg' alt="Icon" width={24} height={24}/>
-            <p>{props.name}</p>
-            <div style={{backgroundColor:props.color}}><p>{props.party}</p></div>
-            <div><p>{props.now}</p></div>
-            <p>{props.num.toString()}</p>
+            <p className="candidateName">{props.name}</p>
+            <div style={{backgroundColor:props.color}} className="candidateParty"><p>{props.party}</p></div>
+            <div className="candidateNow"><p>{props.now}</p></div>
+            <p>{props.num.toString()}票</p>
         </div>
     )
 }
+export function Party(props:PartyProps) {
+    return(
+        <div className="party" style={{backgroundColor:props.color}}>
+            <div className="partyName"><p>{props.name}</p></div>
+            <div className="partyNum"><p>{props.num.toString()}</p></div>
+        </div>
+    )
+}
+
 
 
 
@@ -75,7 +169,7 @@ export default function Election() {
     return(
         <>
             <Header_Login/>
-            <main>
+            <main className="mb-36">
                 <div style={{ width: '100vw', height: '200px', position: 'relative' }}>
                     <Image src="/img/electionView.svg" alt="第27回参議院議員通常選挙" layout="fill" objectFit="contain"/>
                 </div>
@@ -92,11 +186,29 @@ export default function Election() {
                             <p>13:00</p>
                         </div>
                         <h3>現在の順位</h3>
-
-                        {persons.map((person,index) =>{
-                            return <Candidate key={index} {...person} />
-                        })}
-
+                        <div className="candidateBox">
+                            {persons.map((person,index) =>{
+                                return <Candidate key={index} {...person} />
+                            })}
+                        </div>
+                        <div className="candidate Candidate">
+                            <Image src='/img/profile.svg' alt="Icon" width={24} height={24}/>
+                            <p className="candidateName">山口煙管</p>
+                            <div className="candidateParty"><p>無</p></div>
+                            <div className="candidateNow"><p>新</p></div>
+                            <p>5432票</p>
+                        </div>
+                        <div className="PartyBox">
+                            {parties.map((parties,index) => {
+                                return <Party key={index} {...parties}/>
+                            })}
+                        </div>
+                        <p>※選挙情報に誤りがあった場合、恐れ入りますが<Link href="#">こちら</Link>よりお問合せください。</p>
+                        <p>
+                            【候補者・政治家の方へ】<br />
+                            ※政治家・候補者情報の掲載や変更等は無料で承っておりますので、<br />
+                            <Link href="#">こちらをご確認ください。</Link>
+                        </p>
                     </div>
                 </section>
                 <div className="componentsBtn"><Btn label="過去の結果を見る"/></div>
