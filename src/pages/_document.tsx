7c1077bdcba504f9d710ea1ab6ec6a510fc55c36
koudaihirata <2230051@ecc.ac.jp>
2024-01-05 00:29:39 +0900

    もうわからん

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,9 +14,9 @@ class MyDocument extends Document {
                     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-                    <link rel="icon" href="/favicons/favicon.icon" />
                     <link rel="manifest" href="/favicons/site.webmanifest" />
                     <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+                    <link rel="shortcut icon" href="/favicons/favicon.icon" />
                     {/* <link rel="icon" href="data:," /> */}
                     <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
                     <meta name="msapplication-TileColor" content="#da532c" />
