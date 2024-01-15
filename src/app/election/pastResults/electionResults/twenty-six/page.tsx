86673a32f816602b1406d77c72f421d829db5e62
koudaihirata <2230051@ecc.ac.jp>
2024-01-15 16:47:46 +0900

    比例代表選の表示とお問い合わせのフッター部分のページの準備

--- src/app/election/pastResults/electionResults/twenty-six/page.tsx
+++ src/app/election/pastResults/electionResults/twenty-six/page.tsx
@@ -1,11 +1,140 @@
 import Footer_election from "@/components/electionFooter/election";
 import Header_Login from "@/components/header/Login";
 import "@/app/election/pastResults/electionResults/style.scss"
+import Link from "next/link";
+
+type partyNames = {
+    part:string,
+    seatsWon:string,
+    obtainedVotes:string,
+    votingPercentage:string,
+}
 
 
+export function ProportionalRepresentationDistrict(props: { partyNames: partyNames[] }) {
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <td>政党名</td>
+                        <td>獲得<br />議席数</td>
+                        <td>獲得票数</td>
+                        <td>得票率</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.partyNames.map((party, index) => (
+                        <tr key={index}>
+                            <td><Link href="#">{party.part}</Link></td>
+                            <td>{party.seatsWon}</td>
+                            <td>{party.obtainedVotes}</td>
+                            <td>{party.votingPercentage}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
 
 
 export default function twentySix() {
+
+    const partyNames = [
+        {
+            part: '維新政党・新風',
+            seatsWon: '0',
+            obtainedVotes: '65,107',
+            votingPercentage: '0.12',
+        },
+        {
+            part: '幸福実現党',
+            seatsWon: '0',
+            obtainedVotes: '148,020',
+            votingPercentage: '0.28',
+        },
+        {
+            part: '公明党',
+            seatsWon: '6',
+            obtainedVotes: '6,181,431.937',
+            votingPercentage: '11.66',
+        },
+        {
+            part: '国民民主党',
+            seatsWon: '3',
+            obtainedVotes: '3,159,625.890',
+            votingPercentage: '5.96',
+        },
+        {
+            part: '参政党',
+            seatsWon: '1',
+            obtainedVotes: '1,768,385.409',
+            votingPercentage: '3.33',
+        },
+        {
+            part: '新党くにもり',
+            seatsWon: '0',
+            obtainedVotes: '77,861',
+            votingPercentage: '0.15',
+        },
+        {
+            part: '自由民主党',
+            seatsWon: '18',
+            obtainedVotes: '18,256,245.412',
+            votingPercentage: '34.43',
+        },
+        {
+            part: '日本維新の会',
+            seatsWon: '8',
+            obtainedVotes: '7,845,995.352',
+            votingPercentage: '14.8',
+        },
+        {
+            part: '日本共産党',
+            seatsWon: '3',
+            obtainedVotes: '3,618,342.792',
+            votingPercentage: '6.82',
+        },
+        {
+            part: '日本第一党',
+            seatsWon: '0',
+            obtainedVotes: '109,045.614',
+            votingPercentage: '0.21',
+        },
+        {
+            part: 'ごぼうの党',
+            seatsWon: '0',
+            obtainedVotes: '193,724.387',
+            votingPercentage: '0.37',
+        },
+        {
+            part: 'れいわ新選組',
+            seatsWon: '2',
+            obtainedVotes: '2,319,156.016',
+            votingPercentage: '4.37',
+        },
+        {
+            part: '社会民主党',
+            seatsWon: '1',
+            obtainedVotes: '1,258,501.715',
+            votingPercentage: '2.37',
+        },
+        {
+            part: '立憲民主党',
+            seatsWon: '7',
+            obtainedVotes: '6,771,945.011',
+            votingPercentage: '12.77',
+        },
+        {
+            part: 'ＮＨＫ党',
+            seatsWon: '1',
+            obtainedVotes: '1,253,872.467',
+            votingPercentage: '2.36',
+        },
+    ]
+    
+
     return(
         <>
             <Header_Login/>
@@ -55,7 +184,7 @@ export default function twentySix() {
                 </section>
                 <section id="proportionalRepresentationDistrict">
                     <h2>比例代表区</h2>
-
+                    <ProportionalRepresentationDistrict partyNames={partyNames} />
                 </section>
             </main>
             <Footer_election/>
