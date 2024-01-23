74eb635f8a049eb30ff02b688afdebcc78660404
koudaihirata <2230051@ecc.ac.jp>
2024-01-24 01:09:27 +0900

    頼む

--- src/app/election/vote/page.tsx
+++ src/app/election/vote/page.tsx
@@ -1,9 +1,9 @@
-"use client"
-
-import Vote from "@/components/Vote/Vote";
+import dynamic from 'next/dynamic';
 import Footer_election from "@/components/electionFooter/election"
 import Header_Login from "@/components/header/Login"
 import { BrowserRouter as Router } from "react-router-dom";
+
+const Vote = dynamic(() => import('@/components/Vote/Vote'), { ssr: false });
 
 export default function VoteMain() {
     return(
