1e0bc675da42eb210cbab55b90ca493e7ab90abe
koudaihirata <2230051@ecc.ac.jp>
2024-01-27 23:40:44 +0900

    一応完成

--- src/app/poem/Cumulative/page.tsx
+++ src/app/poem/Cumulative/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import PoemFooter from "@/components/poemFooter/poemFooter"
-import "@/app/poem/style.scss"
 import Poemer from "@/components/Poemer/Poemer"
+import "@/app/poem/style.scss"
+import PoemFooter from "@/components/poemFooter/poemFooter"
 import Link from "next/link"
 
 
@@ -15,7 +15,8 @@ const Poemers = [
         dey: "2024.1.25",
         author: "選挙太郎",
         good: 105,
-        poem: 'あなたと出会ってから<br />私の世界は色づいた<br />あなたの笑顔に癒され<br />あなたの言葉に励まされ<br /><br />あなたと歩んだ道は<br />私の宝物だった<br />あなたと見た景色は<br />私の記憶に刻まれた<br /><br />でも今日は別れの日<br />あなたは選挙に出ると言った<br />私は涙をこらえて<br />あなたを見送ると決めた<br /><br />あなたに伝えたいことは<br />たくさんあるけど<br />言葉にできないから<br />この詩に込めた<br /><br />あなたが勝利することを<br />心から願っています<br />あなたが帰ってくる日まで<br />私はここで待っています',
+        poem: "あなたと出会ってから",
+        aaa: "あなたと出会ってから",
     },
     {
         img: "/img/tapestryTwo.svg",
@@ -26,6 +27,7 @@ const Poemers = [
         author: "板倉代",
         good: 84,
         poem: 'あなたと出会ってから<br />私の世界は色づいた<br />あなたの笑顔に癒され<br />あなたの言葉に励まされ<br /><br />あなたと歩んだ道は<br />私の宝物だった<br />あなたと見た景色は<br />私の記憶に刻まれた<br /><br />でも今日は別れの日<br />あなたは選挙に出ると言った<br />私は涙をこらえて<br />あなたを見送ると決めた<br /><br />あなたに伝えたいことは<br />たくさんあるけど<br />言葉にできないから<br />この詩に込めた<br /><br />あなたが勝利することを<br />心から願っています<br />あなたが帰ってくる日まで<br />私はここで待っています',
+        aaa: "あなたと出会ってから",
     },
     {
         img: "/img/tapestryThree.svg",
@@ -60,7 +62,8 @@ const Poemers = [
 
 ]
 
-export default function Poem() {
+export default function Cumulative() {
+
     return(
         <>
             <main className="PoemMain">
@@ -68,13 +71,13 @@ export default function Poem() {
                     <h2>選挙ポエム</h2>
                 </section>
                 <ul>
-                    <li><Link href="#">新着</Link></li>
+                    <li><Link href="/poem" style={{background: "#fff",color: "#333"}}>新着</Link></li>
                     <li><Link href="/poem/week">週間</Link></li>
                     <li><Link href="/poem/monthly">月間</Link></li>
                     <li><Link href="/poem/yearly">年間</Link></li>
-                    <li><Link href="/poem/Cumulative">累計</Link></li>
+                    <li><Link href="#" style={{background: "#68c1f2",color: "#fff"}}>累計</Link></li>
                 </ul>
-                {Poemers.map((poem,index) => {
+                {Poemers.map((poem, index) => {
                     return <Poemer key={index} {...poem}/>
                 })}
                 <p><Link href="#">作品をもっと見る</Link></p>
