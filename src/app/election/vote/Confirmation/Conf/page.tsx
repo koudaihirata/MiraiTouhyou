ea9c46bbe7ee845856f4505915b0bab33c07816f
koudaihirata <2230051@ecc.ac.jp>
2024-02-21 10:12:42 +0900

    投票完了後の自動画面遷移

--- src/app/election/vote/Confirmation/Conf/page.tsx
+++ src/app/election/vote/Confirmation/Conf/page.tsx
@@ -2,15 +2,25 @@
 
 import Footer_election from "@/components/electionFooter/election";
 import Link from "next/link";
-
+import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
 
 
 export default function Conf() {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+    }, [router]);
+    
+
     return(
         <>
             <main style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '100%', height: '100vh', gap: '32px'}}>
                 <p style={{fontSize: '16px', textAlign: 'center'}}>投票完了しました</p>
-                <Link href="/"style={{fontSize: '16px', color: '#6464FF', textDecoration: 'underline'}}>ホームに戻る</Link>
+                <p style={{fontSize: '16px', color: '#6464FF'}}>3秒後に戻ります</p>
             </main>
             <Footer_election/>
         </>
